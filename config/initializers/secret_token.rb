b512026da3c579d932a3c008772eedbaf41356b3
Alvin Ghouas <alvin.ghouas@gmail.com>
2013-10-18 10:35:28 +0200

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lister2::Application.config.secret_key_base = '289061b893e6b91cf6f161ca68532a5f5ca3e16661285c87b418a2c61af081865427b9e9f217d2c9ea1196db71bd5e3dfc93a81063b8432a6d025b652ae9187f'
